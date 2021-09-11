52f5444cb5bd47e14433ed24b097b49e71b6415c
Sergei Stadnik <sergeisqa@gmail.com>
2021-09-11 04:35:14 -0700

    upd

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 
+	"github.com/BurntSushi/toml"
 	"github.com/monkrus/rabbitaxi.git/internal/app/apiserver"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
 func main() {
-	s := apiserver.New()
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
